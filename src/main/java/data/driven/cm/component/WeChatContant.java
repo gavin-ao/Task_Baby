a06210ee0679c825e2ac27206e8ca74a88af392f
logan <leankyPublic@qq.com>
2018-11-15 15:39:39 +0800

    增加EventKey 常量
    Signed-off-by: logan <leankyPublic@qq.com>

--- src/main/java/data/driven/cm/component/WeChatContant.java
+++ src/main/java/data/driven/cm/component/WeChatContant.java
@@ -36,4 +36,5 @@ public class WeChatContant {
     public static final String Event = "Event";
     public static final String MediaId = "MediaId";
     public static final String MEDIA_ID= "media_id"; //用于得到返回临时素材的media_id
+    public static final String EventKey = "EventKey";
 }
