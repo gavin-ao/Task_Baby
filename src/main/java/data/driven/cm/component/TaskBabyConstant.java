5c50385f9de911925969a17319d97c7630e23ec9
logan <leankyPublic@qq.com>
2018-11-15 18:46:24 +0800

    提交常量文件
    Signed-off-by: logan <leankyPublic@qq.com>

---
+++ src/main/java/data/driven/cm/component/TaskBabyConstant.java
@@ -0,0 +1,15 @@
+package data.driven.cm.component;
+
+/**
+ * @program: Task_Baby
+ * @description: 任务宝常量
+ * @author: Logan
+ * @create: 2018-11-15 17:13
+ **/
+
+public class TaskBabyConstant {
+    public static final String KEY_POSTER_URL="POSTER_URL";
+    public static final String KEY_QRCODE_RL="QRCODE_URL";
+    public static final String SEPERATOR_QRSCEAN="&&";
+
+}
