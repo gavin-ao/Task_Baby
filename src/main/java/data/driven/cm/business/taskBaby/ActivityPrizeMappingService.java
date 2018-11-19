ed10b9b0250784329c805ef194c6c9cfa657a8c1
liuxiaolei319 <182150305@qq.com>
2018-11-19 17:01:23 +0800

    增加: 得到活动奖品关联表实体，只取一个 接口 获取所剩的奖品数  接口 刘晓磊 2018.11.19

--- src/main/java/data/driven/cm/business/taskBaby/ActivityPrizeMappingService.java
+++ src/main/java/data/driven/cm/business/taskBaby/ActivityPrizeMappingService.java
@@ -1,5 +1,7 @@
 package data.driven.cm.business.taskBaby;
 
+import data.driven.cm.entity.taskBaby.ActivityPrizeMappingEntity;
+
 /**
  * @Author: lxl
  * @describe 活动奖品关联表Service
@@ -7,4 +9,18 @@ package data.driven.cm.business.taskBaby;
  * @Version 1.0
  */
 public interface ActivityPrizeMappingService {
+
+    /**
+     * 得到活动奖品关联表实体，只取一个
+     * @param actId 活动Id
+     * @return
+     */
+    public ActivityPrizeMappingEntity getEntityByActId(String actId);
+
+    /**
+     * 获取所剩的奖品数
+     * @param actId 活动Id
+     * @return
+     */
+    public Integer getRemainderPrize(String actId);
 }
--- src/main/java/data/driven/cm/business/taskBaby/impl/ActivityPrizeMappingServiceImpl.java
+++ src/main/java/data/driven/cm/business/taskBaby/impl/ActivityPrizeMappingServiceImpl.java
@@ -1,10 +1,48 @@
 package data.driven.cm.business.taskBaby.impl;
 
+import data.driven.cm.business.taskBaby.ActivityPrizeMappingService;
+import data.driven.cm.dao.JDBCBaseDao;
+import data.driven.cm.entity.taskBaby.ActivityPrizeMappingEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * @Author: lxl
  * @describe 活动奖品关联表Impl
  * @Date: 2018/11/19 16:23
  * @Version 1.0
  */
-public class ActivityPrizeMappingServiceImpl {
+public class ActivityPrizeMappingServiceImpl implements ActivityPrizeMappingService{
+    @Autowired
+    JDBCBaseDao jdbcBaseDao;
+
+    /**
+     * 得到活动奖品关联表实体，只取一个
+     * @param actId 活动Id
+     * @return
+     */
+    @Override
+    public ActivityPrizeMappingEntity getEntityByActId(String actId) {
+        String sql = "select prize_id,act_id,token,link_url,token_status,creator,create_at from activity_prize_mapping where act_id = ? and token_status = 0 limit 1";
+        List<ActivityPrizeMappingEntity> activityPrizeMappingEntityList = jdbcBaseDao.queryList(ActivityPrizeMappingEntity.class,sql,actId);
+        if (activityPrizeMappingEntityList != null && activityPrizeMappingEntityList.size() > 0){
+            ActivityPrizeMappingEntity activityPrizeMappingEntity = activityPrizeMappingEntityList.get(0);
+            String updateSql = "update activity_prize_mapping set token_status = 1 where prize_id = ?";
+            jdbcBaseDao.executeUpdate(updateSql,activityPrizeMappingEntity.getPrizeId());
+            return activityPrizeMappingEntity;
+        }
+        return null;
+    }
+
+    /**
+     * 获取所剩的奖品数
+     * @param actId 活动Id
+     * @return
+     */
+    @Override
+    public Integer getRemainderPrize(String actId) {
+        String sql = "select count(1) as remainder_count from activity_prize_mapping where act_id = ? and token_status = 0 ";
+        return jdbcBaseDao.getCount(sql,actId);
+    }
 }
