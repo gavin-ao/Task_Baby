19193e9b1137f15671c8f3aea8ee4c457709fedb
liuxiaolei319 <182150305@qq.com>
2018-11-19 10:30:38 +0800

    增加活动奖励表接口：  1.新增活动奖励信息。  2.获取活动奖励实体，通过活动Id和用户OpenId。  3.获取活动奖励Id,通过活动Id和用户OpenId。
    刘晓磊 2018.11.17

---
+++ src/main/java/data/driven/cm/business/taskBaby/ActivityRewardService.java
@@ -0,0 +1,30 @@
+package data.driven.cm.business.taskBaby;
+
+import data.driven.cm.entity.taskBaby.ActivityRewardEntity;
+
+/**
+ * @Author: lxl
+ * @describe 活动奖励表Service
+ * @Date: 2018/11/19 9:48
+ * @Version 1.0
+ */
+public interface ActivityRewardService {
+
+    /**
+     *  新增 活动奖励信息
+     * @param actId 活动ID
+     * @param wechatUserId 微信用户Id
+     * @param wechatAccount 公众号原始ID
+     * @param receiveStatus 领取状态,1 已领取 0 未领取
+     * @return
+     */
+    public String insertActivityRewardEntity(String actId,String wechatUserId,String wechatAccount,Integer receiveStatus);
+
+    /**
+     * 获取 活动奖励实体
+     * @param actId 活动Id
+     * @param wechatUserId 用户OpenId
+     * @return ActivityRewardEntity
+     */
+    public ActivityRewardEntity getEntityByActIdAndWechatUserId(String actId, String wechatUserId);
+}
---
+++ src/main/java/data/driven/cm/business/taskBaby/impl/ActivityRewardServiceImpl.java
@@ -0,0 +1,74 @@
+package data.driven.cm.business.taskBaby.impl;
+
+import data.driven.cm.business.taskBaby.ActivityRewardService;
+import data.driven.cm.dao.JDBCBaseDao;
+import data.driven.cm.entity.taskBaby.ActivityRewardEntity;
+import data.driven.cm.util.UUIDUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: lxl
+ * @describe 活动奖励表Impl
+ * @Date: 2018/11/19 9:52
+ * @Version 1.0
+ */
+public class ActivityRewardServiceImpl implements ActivityRewardService {
+
+    @Autowired
+    JDBCBaseDao jdbcBaseDao;
+
+    /**
+     *  新增 活动奖励信息
+     * @param actId 活动ID
+     * @param wechatUserId 微信用户Id openId
+     * @param wechatAccount 公众号原始ID
+     * @param receiveStatus 领取状态,1 已领取 0 未领取
+     * @return
+     */
+    @Override
+    public String insertActivityRewardEntity(String actId, String wechatUserId, String wechatAccount, Integer receiveStatus) {
+        Date caretAt = new Date();
+        String rewardId = getRewardIdByActIdAndWechatUserId(actId,wechatUserId);
+        if (rewardId == null){
+            rewardId = UUIDUtil.getUUID();
+            String sql = "insert into activity_reward (reward_id,act_id,wechat_user_id,wechat_account,receive_status,create_at) VALUES (?,?,?,?,?,?)";
+            jdbcBaseDao.executeUpdate(sql,rewardId,actId,wechatUserId,wechatAccount,receiveStatus,caretAt);
+        }
+        return rewardId;
+    }
+
+    /**
+     *  获取活动奖励Id
+     * @param actId 任务Id
+     * @param wechatUserId 用户OpeinId
+     * @return
+     */
+    public String getRewardIdByActIdAndWechatUserId(String actId,String wechatUserId){
+        String sql = "select reward_id from activity_reward where act_id = ? and wechat_user_id = ? ";
+        Object id = jdbcBaseDao.getColumn(sql,actId,wechatUserId);
+        if (StringUtils.isNotEmpty(id.toString())){
+            return id.toString();
+        }
+        return null;
+    }
+
+    /**
+     * 获取 活动奖励实体
+     * @param actId 活动Id
+     * @param wechatUserId 用户OpenId
+     * @return ActivityRewardEntity
+     */
+    @Override
+    public ActivityRewardEntity getEntityByActIdAndWechatUserId(String actId,String wechatUserId){
+        String sql = "select reward_id,act_id,wechat_user_id,wechat_account,receive_status,create_at from activity_reward where act_id = ? and wechat_user_id = ?";
+        List<ActivityRewardEntity> activityRewardEntityList = jdbcBaseDao.queryList(ActivityRewardEntity.class,sql,actId,wechatUserId);
+        if (activityRewardEntityList != null && activityRewardEntityList.size() > 0){
+            return activityRewardEntityList.get(0);
+        }
+        return null;
+    }
+}
