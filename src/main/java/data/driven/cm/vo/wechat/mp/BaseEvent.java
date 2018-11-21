c6ef509bd360ab3fd0f71c2b2ca7ee0ed67317c5
logan <leankyPublic@qq.com>
2018-11-21 09:35:50 +0800

    增加微信消息事件实体
    Signed-off-by: logan <leankyPublic@qq.com>

--- src/main/java/data/driven/cm/vo/wechat/mp/BaseEvent.java
+++ src/main/java/data/driven/cm/vo/wechat/mp/BaseEvent.java
@@ -23,4 +23,43 @@ public class BaseEvent {
     //事件类型
     private String Event;
 
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+
+    public long getCreateTime() {
+        return CreateTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        CreateTime = createTime;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
+
+    public String getEvent() {
+        return Event;
+    }
+
+    public void setEvent(String event) {
+        Event = event;
+    }
 }
