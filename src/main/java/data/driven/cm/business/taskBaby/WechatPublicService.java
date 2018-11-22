eeca274ac7f3f343f2fc8327bc7307dea3b34fd1
liuxiaolei319 <182150305@qq.com>
2018-11-22 17:37:21 +0800

    增加 通过用户appid 和 主键id查找微信公众号信息表实体两个方法
    刘晓磊 2018.11.22

--- src/main/java/data/driven/cm/business/taskBaby/WechatPublicService.java
+++ src/main/java/data/driven/cm/business/taskBaby/WechatPublicService.java
@@ -17,4 +17,18 @@ public interface WechatPublicService {
      * @return WechatPublicEntity 实体
      */
     public WechatPublicEntity getEntityByWechatAccount(String wechatAccount);
+
+    /**
+     * 通过 authorizationAppid(开发者ID)得到微信公众号信息表实体类
+     * @param authorizationAppid
+     * @return WechatPublicEntity
+     */
+    public WechatPublicEntity getEntityByAuthorizationAppid(String authorizationAppid);
+
+    /**
+     * 通过 wechat_public_id(微信公众号信息表id)得到微信公众号信息表实体类
+     * @param wechatPublicId 微信公众号信息表id
+     * @return WechatPublicEntity
+     */
+    public WechatPublicEntity getEntityByWechatPublicId(String wechatPublicId);
 }
--- src/main/java/data/driven/cm/business/taskBaby/impl/WechatPublicServiceImpl.java
+++ src/main/java/data/driven/cm/business/taskBaby/impl/WechatPublicServiceImpl.java
@@ -20,6 +20,39 @@ public class WechatPublicServiceImpl implements WechatPublicService{
 
     @Autowired
     private JDBCBaseDao jdbcBaseDao;
+
+    /**
+     * 通过 authorizationAppid(开发者ID)得到微信公众号信息表实体类
+     * @param authorizationAppid
+     * @return
+     */
+    @Override
+    public WechatPublicEntity getEntityByAuthorizationAppid(String authorizationAppid) {
+        String sql = "select wechat_public_id,authorization_appid,func_info,create_at from wechat_public where authorization_appid = ?";
+        List<WechatPublicEntity> wechatPublicEntityList = jdbcBaseDao.queryList(WechatPublicEntity.class,sql,
+                authorizationAppid);
+        if (wechatPublicEntityList != null && wechatPublicEntityList.size() > 0) {
+            return wechatPublicEntityList.get(0);
+        }
+        return null;
+    }
+
+    /**
+     * 通过 wechat_public_id(微信公众号信息表id)得到微信公众号信息表实体类
+     * @param wechatPublicId 微信公众号信息表id
+     * @return WechatPublicEntity
+     */
+    @Override
+    public WechatPublicEntity getEntityByWechatPublicId(String wechatPublicId) {
+        String sql = "select wechat_public_id,authorization_appid,func_info,create_at from wechat_public where wechat_public_id = ?";
+        List<WechatPublicEntity> wechatPublicEntityList = jdbcBaseDao.queryList(WechatPublicEntity.class,sql,wechatPublicId);
+        if (wechatPublicEntityList != null && wechatPublicEntityList.size() > 0){
+            return wechatPublicEntityList.get(0);
+        }
+        return null;
+    }
+
+
     /**
      * 通过 wechatAccount(原始Id)得到微信公众号实体类
      * @param wechatAccount wechatAccount(原始Id)
