22e9dba383b8ca36c707b74f41306c8f727e5948
liuxiaolei319 <182150305@qq.com>
2018-11-23 15:37:57 +0800

    增加得到扫码URL
    刘晓磊 2018.11.23

--- src/main/java/data/driven/cm/controller/TaskBaby/ThirdPartyController.java
+++ src/main/java/data/driven/cm/controller/TaskBaby/ThirdPartyController.java
@@ -1,11 +1,20 @@
 package data.driven.cm.controller.TaskBaby;
 
+import data.driven.cm.business.taskBaby.ThirdPartyService;
+import data.driven.cm.component.WeChatConstant;
+import data.driven.cm.util.WeChatUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * @Author: lxl
@@ -13,25 +22,49 @@ import javax.servlet.http.HttpServletResponse;
  * @Date: 2018/11/23 9:21
  * @Version 1.0
  */
+@Controller
+@RequestMapping(path = "/thirdParty")
 public class ThirdPartyController {
+    Logger logger = LoggerFactory.getLogger(ThirdPartyController.class);
+
+    @Autowired
+    ThirdPartyService thirdPartyService;
 
 
     /**
      * 微信第三方授权事件的接收
      */
-    @RequestMapping(value="/authorize",method = {RequestMethod.GET,RequestMethod.POST})
+    @RequestMapping(value = "/authorize", method = {RequestMethod.GET, RequestMethod.POST})
     public void acceptAuthorizeEvent(HttpServletRequest request,
-                                     HttpServletResponse response){
-        try{
-
-        }catch (Exception e){
+                                     HttpServletResponse response) {
+        try {
+            logger.info("----------------------进入authorize----------------------");
+            thirdPartyService.hadleAuthorize(request, response);
+            PrintWriter pw = response.getWriter();
+            pw.write("success");
+            pw.flush();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    /**
+     * 得到扫码URL
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getPreAuthCode", method = {RequestMethod.GET})
+    public String getPreAuthCode(){
+        String componentloginpageURL ="https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=%s&pre_auth_code=%s&redirect_uri=%s&auth_type=3";
+        String preAuthCode = WeChatUtil.getPreAuthCode();
+        String.format(componentloginpageURL, WeChatConstant.THIRD_PARTY_APPID,preAuthCode,"http://easy7share.com/thirdParty/authcallback");
+        return componentloginpageURL;
+    }
+
     public void authorizeCallback(@RequestParam(value="autho_code") String authCode,
                                   @RequestParam(value="expires_in") String expriesIn){
-        
+
     }
+
 }
