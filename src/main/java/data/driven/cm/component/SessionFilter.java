5c2d7e4bf349ebe5b1499e0475b2af24a5a5ebfa
liuxiaolei319 <182150305@qq.com>
2018-11-23 15:40:23 +0800

    增加微信第三方平台过滤器
    刘晓磊 2018.11.23

--- src/main/java/data/driven/cm/component/SessionFilter.java
+++ src/main/java/data/driven/cm/component/SessionFilter.java
@@ -31,6 +31,8 @@ public class SessionFilter implements Filter{
         EXCLUDE_URL_SET.add("/static");
         //微信接口不需要过滤
         EXCLUDE_URL_SET.add("/wechatapi");
+        //微信第三方平台
+        EXCLUDE_URL_SET.add("/thirdParty");
     }
 
     @Override
