6e4795e73a2e8f7a1ae46ba45bb9b003960c3e88
logan <leankyPublic@qq.com>
2018-12-21 12:33:17 +0800

    根据actid,fromUnionId,toUnionId 查找还未助力的实体

--- src/main/java/data/driven/cm/business/taskbaby/ActivityHelpService.java
+++ src/main/java/data/driven/cm/business/taskbaby/ActivityHelpService.java
@@ -1,5 +1,7 @@
 package data.driven.cm.business.taskbaby;
 
+import data.driven.cm.entity.taskbaby.ActHelpEntity;
+
 import java.util.Map;
 
 /**
@@ -95,6 +97,18 @@ public interface ActivityHelpService {
      * @return activityCompletionNumber 任务完成人数
      */
     Integer getActivityCompletionNumber(String actId);
+
+    /**
+    * 查找当前活动中,助力者还未成功助力的ActHelpEntity
+    * @author Logan
+    * @date 2018-12-21 12:26
+    * @param actId
+    * @param masterOpenId
+    * @param detailOpenId
+
+    * @return
+    */
+    ActHelpEntity getHelpEntityOfNoneHelpDetail(String actId,String masterOpenId,String detailOpenId);
 
 
 
