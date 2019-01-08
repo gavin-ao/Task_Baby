4e5125585107cb258f37328b72285d347a98b3fb
liuxiaolei319 <182150305@qq.com>
2019-01-08 10:11:26 +0800

    修改授权回调URL lxl 2019.01.08

---
+++ src/main/java/data/driven/cm/entity/taskbaby/CustomerConfigureEntity.java
@@ -0,0 +1,48 @@
+package data.driven.cm.entity.taskbaby;
+
+/**
+ * @Author: lxl
+ * @describe 客服配置Entity
+ * @Date: 2019/1/8 9:51
+ * @Version 1.0
+ */
+public class CustomerConfigureEntity {
+    /**
+     * 主键
+     */
+    private String customerConfigureId;
+
+    /**
+     * 系统用户id外键
+     */
+    private String sysUserId;
+    /**
+     * 消息显示顺序
+     */
+    private Integer order;
+
+    /**
+     *
+     */
+    private String keyWord;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
